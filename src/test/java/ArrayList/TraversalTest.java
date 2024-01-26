bbf5186b0dd54c2d1d21dfe155efb6bf395f8f6c
gakrish5 <axg17750@ucmo.edu>
2024-01-26 10:13:20 -0600

    Added JUnit test cases for ArrayList

---
+++ src/test/java/ArrayList/TraversalTest.java
@@ -0,0 +1,36 @@
+package ArrayList;
+
+import org.testng.annotations.Test;
+import java.util.ArrayList;
+
+public class TraversalTest {
+
+    @Test
+    public void test1() {
+        ArrayList<String> emptyList = new ArrayList<>();
+
+        ArrayListTraversal.traverseWithForLoop(emptyList);
+
+        ArrayListTraversal.traverseWithEnhancedForLoop(emptyList);
+    }
+
+    @Test
+    public void test2() {
+        ArrayList<String> stringList = new ArrayList<>();
+        stringList.add("One");
+        stringList.add("Two");
+        stringList.add("Three");
+
+        ArrayListTraversal.traverseWithForLoop(stringList);
+    }
+
+    @Test
+    public void test3() {
+        ArrayList<String> stringList = new ArrayList<>();
+        stringList.add("Apple");
+        stringList.add("Banana");
+        stringList.add("Orange");
+
+        ArrayListTraversal.traverseWithEnhancedForLoop(stringList);
+    }
+}
